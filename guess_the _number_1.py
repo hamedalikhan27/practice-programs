617cadd8caa4085b7c5a0807e525a40c5142d1da
unknown <hamedalikhan27@gmail.com>
2023-06-11 09:43:54 +0530

    added second program

--- guess_the _number_1.py
+++ guess_the _number_1.py
@@ -4,7 +4,7 @@ def guess(x):
     random_number = random.randint(1,x)
     guess = 0
     while guess != random_number:
-        guess = int(input(f" Guess a number between 1 to {x}: "))
+        guess = int(input(f" Guess a number between 1 to {x}:"))
         if guess < random_number:
             print(f"wrong answer, guess again, too low")
         elif guess > random_number:
